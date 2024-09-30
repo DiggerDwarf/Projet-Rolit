bbd543d1fe2a73931c8b31f23476d575f211461f
Jeremy Li <jeremyli2906@hotmail.com>
2024-09-30 11:49:22 +0200

    First optimization

--- main.py
+++ main.py
@@ -8,24 +8,23 @@ WIDTH, HEIGHT = 8, 8
 VIDE, ROUGE, VERT, JAUNE, BLEU = 0, 1, 2, 3, 4
 
 colors = {
-    VIDE: colorama.Back.BLACK,
-    ROUGE: colorama.Back.RED,
-    VERT: colorama.Back.GREEN,
-    JAUNE: colorama.Back.YELLOW,
-    BLEU: colorama.Back.BLUE
+    VIDE: f"{colorama.Back.BLACK}  {colorama.Back.RESET}",
+    ROUGE: f"{colorama.Back.RED}  {colorama.Back.RESET}",
+    VERT: f"{colorama.Back.GREEN}  {colorama.Back.RESET}",
+    JAUNE: f"{colorama.Back.YELLOW}  {colorama.Back.RESET}",
+    BLEU: f"{colorama.Back.BLUE}  {colorama.Back.RESET}"
 }
 
 
 def afficher_grille(grille):
     for row in grille:
         for elem in row:
-            print(colors[elem] + "  " + colorama.Back.RESET, end="")
+            print(colors[elem], end="")
         print()
 
 def main():
     grille = [[randint(VIDE, BLEU) for _ in range(WIDTH)] for _ in range(HEIGHT)]
     afficher_grille(grille)
-    
     
 
 if __name__ == "__main__":
